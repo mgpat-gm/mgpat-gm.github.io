10992b1bf5691a5bb1b7f32a55bd4c135150451d
mgpat-gm <mgpat-gm@pm.me>
2020-07-05 02:53:33 -0400

    banner changes

--- layout.js
+++ layout.js
@@ -2,14 +2,14 @@ with (document) {
 
 writeln('<div class="banner">');
 writeln('<p>');
-writeln(' Browse Site:');
+writeln(' Ghast\'s Grotto:');
 writeln('<br>');
 writeln('<br>');
 writeln('<a title="News, updates, and more!" href="index.html">Home</a>');
-writeln('<a title="My Diablo 1 characters and their items" href="characters.html">Characters</a>');
-writeln('<a title="A list of all the uniques I have collected so far" href="uniques.html">My Uniques</a>');
-writeln('<a title="Diablo Item Price/Rarity Calculators, created by Eso aka the_Langolier" href="calcs.html">Item Calcs</a>');
-writeln('<a title="Self explanatory" href="shrinelist.html">Shrine List</a>');
+writeln('<a title="My characters and their items" href="characters.html">Characters</a>');
+writeln('<a title="A list of all the uniques I have collected so far" href="uniques.html">Uniques</a>');
+writeln('<a title="Item Price/Rarity Calculators, created by Eso aka the_Langolier" href="calcs.html">Calcs</a>');
+writeln('<a title="Self explanatory" href="shrinelist.html">Shrines</a>');
 writeln('<a title="A list of oldies(but goodies) and newby Diablo related sites and forums" href="links.html">Links</a>');
 writeln('</script>');
 writeln('</div>');
